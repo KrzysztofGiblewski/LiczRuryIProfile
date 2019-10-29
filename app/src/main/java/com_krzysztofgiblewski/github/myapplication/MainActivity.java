ecd2fc78fab4f755b268f883ae04457b4d6878f2
Krzysztof Giblewski <krzysztofgiblewski@gmail.com>
2019-10-29 20:26:03 +0100

    pierwsza klasa licz

--- app/src/main/java/com_krzysztofgiblewski/github/myapplication/MainActivity.java
+++ app/src/main/java/com_krzysztofgiblewski/github/myapplication/MainActivity.java
@@ -4,9 +4,13 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    double pierwsza;
+    String wynik = "jakiś tekst";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,10 +22,15 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "jakiś tekst", Toast.LENGTH_LONG).show();
+                licz();
+                Toast.makeText(getApplicationContext(), wynik, Toast.LENGTH_LONG).show();
             }
         });
     }
 
-
+    private void licz() {
+        EditText DlugoscOdcinka = (EditText) findViewById(R.id.editTextDlugoscOdcinka);
+        pierwsza = Double.valueOf(DlugoscOdcinka.getText().toString());
+        wynik = String.valueOf(pierwsza);
+    }
 }
